1d7be5a23755d19b1394e70bfbdc599a033f7096
Anup <anupgoshal9367@yahoo.com>
2023-05-30 15:34:57 +0530

    all categoy page display sub sub catgeoy

--- resources/js/pages/AllCategories.vue
+++ resources/js/pages/AllCategories.vue
@@ -28,12 +28,25 @@
                                     <router-link :to="{ name: 'Category', params: {categorySlug: category.slug}}" class="text-reset">{{ category.name }}</router-link>
                                 </h3>
                                 <div v-if="category.children.data.length">
-                                    <v-hover v-slot="{ hover }" v-for="(children, j) in category.children.data" :key="j">
+                                    <!-- <v-hover v-slot="{ hover }" v-for="(children, j) in category.children.data" :key="j">
                                         <router-link
                                             :class="['text-reset me-1 opacity-80', {'primary--text text-decoration-underline':hover}]"
                                             :to="{ name: 'Category', params: {categorySlug: children.slug}}"
                                         >{{ children.name }}<span v-if="j+1 !== category.children.data.length" class="">,</span></router-link>
-                                    </v-hover>
+                                    </v-hover> -->
+
+                                    <ul v-if="category.children.data.length != 0"  aria-labelledby="dropdownMenu1" >
+                                        <li v-for="(subcategory, subIndex) in category.children.data" :key="subIndex" :class="{'subsubmenuitems': subcategory.childrensub.data.length != 0}">
+                                            <router-link :to="{ name: 'Category', params: {categorySlug: subcategory.slug}}">{{ subcategory.name }}</router-link>
+
+                                            <ul v-if="subcategory.childrensub.data.length != 0"  aria-labelledby="dropdownMenu1" >
+                                                <li v-for="(subsubcategory, subsubIndex) in subcategory.childrensub.data" :key="subsubIndex">
+                                                    <router-link :to="{ name: 'Category', params: {categorySlug: subsubcategory.slug}}">{{ subsubcategory.name }}</router-link>
+                                                </li>
+                                            </ul>
+
+                                        </li>
+                                    </ul>
                                 </div>
                             </v-col>
                         </v-row>
